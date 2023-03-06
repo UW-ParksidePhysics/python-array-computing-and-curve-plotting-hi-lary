00fa67b6ba767b2f0489c602dff6aa3073c297d6
hi-lary <beansy7@sbcglobal.net>
2023-03-06 16:50:51 -0600

    homework

---
+++ fill_arrays_loop.py
@@ -0,0 +1,8 @@
+# exercise 5.2
+import numpy as np
+x_values_array = np.linspace(-4, 4, 41)  # starting value, ending value, how many values
+x_array = np.array()
+y_array = np.array()
+for x in x_values_array:
+    x_array.append(x)
+print(x_array)
---
+++ fill_lists.py
@@ -0,0 +1,13 @@
+# exercise 5.1
+from math import pi, exp, sqrt
+import numpy as np
+
+x_values_array = np.linspace(-4, 4, 41)  # starting value, ending value, how many values
+xlist = []
+hlist = []
+
+for x in x_values_array:
+    xlist.append(x)
+    h = (1 / sqrt(2 * pi)) * exp((-1 / 2) * x ** 2)
+    hlist.append(h)
+    print(f'{x}, {h}')
